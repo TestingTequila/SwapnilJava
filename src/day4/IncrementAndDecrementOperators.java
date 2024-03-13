1b4b01c0c7d9b003f7bf98c1f68fdc8ddbdc2607
Ashish Mishra <ashish.mishra.sse@gmail.com>
2024-03-13 09:54:52 +0530

    Increment/Decrement Operator

---
+++ src/day4/IncrementAndDecrementOperators.java
@@ -0,0 +1,45 @@
+package day4;
+
+public class IncrementAndDecrementOperators
+{
+    public static void main(String[] args) {
+        //1. --pre decrement
+        int a = 1;
+        int b = --a;
+        System.out.println(a); // 0
+        System.out.println(b); // 0
+
+        int c =- 99;
+        int d = --c;
+        System.out.println(c); // -100
+        System.out.println(d); // -100
+
+        int x =10;
+        int y = --x;
+        System.out.println(x);//9
+        System.out.println(y);//9
+
+        int num =10;
+        System.out.println(--num);// println= --num  --> 9
+        System.out.println(num);  // 9
+
+        float f =2.5f;
+        float j =f++;
+        System.out.println(f);//3.5
+        System.out.println(j);//2.5
+
+        char ch = 'a';
+        //System.out.println(++ch);//
+        //System.out.println(ch); //
+
+        int m = 200;
+        int n = m++;
+        System.out.println(--m);
+        System.out.println(m);
+        System.out.println(m++);
+        System.out.println(m--);
+        System.out.println(--m);
+        System.out.println(m);
+    }
+
+}
